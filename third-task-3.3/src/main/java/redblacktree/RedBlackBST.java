1158bedbc4ba3662c77d34bb5d12b2048487ecca
Azro_Sk <azro.skib08@gmail.com>
2023-03-23 01:56:37 +0300

    3.3-3

---
+++ third-task-3.3/src/main/java/redblacktree/RedBlackBST.java
@@ -0,0 +1,128 @@
+package redblacktree;
+
+import edu.princeton.cs.introcs.StdOut;
+
+public class RedBlackBST<K extends Comparable<K>, V> {
+    private static final boolean RED = true;
+    private static final boolean BLACK = false;
+    private Node root;
+
+    private class Node {
+        K key;
+        V value;
+        Node left;
+        Node right;
+        boolean color;
+
+        public Node(K key, V value, boolean color) {
+            this.key = key;
+            this.value = value;
+            this.color = color;
+        }
+    }
+    private boolean isRed(Node node) {
+        if (node == null) {
+            return false;
+        }
+        return node.color == RED;
+    }
+
+    public void add(K key, V val) {
+        root = add(root, key, val);
+        root.color = BLACK;
+    }
+
+    private Node add(Node node, K key, V value) {
+        if (node == null) return new Node(key, value, RED);
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) node.left = add(node.left, key, value);
+        else if (cmp > 0) node.right = add(node.right, key, value);
+        else node.value = value;
+
+
+        if (isRed(node.right) && !isRed(node.left)) node = rotateLeft(node);
+        if (isRed(node.left) && isRed(node.left.left)) node = rotateRight(node);
+        if (isRed(node.left) && isRed(node.right)) flipColors(node);
+
+        return node;
+    }
+
+    private Node rotateLeft(Node node) {
+        Node rotateNode = node.right;
+        node.right = rotateNode.left;
+        rotateNode.left = node;
+        rotateNode.color = node.color;
+        node.color = RED;
+        return rotateNode;
+    }
+
+    private Node rotateRight(Node node) {
+        Node rotateNode = node.left;
+        node.left = rotateNode.right;
+        rotateNode.right = node;
+        rotateNode.color = node.color;
+        node.color = RED;
+        return rotateNode;
+    }
+
+    private void flipColors(Node node) {
+        node.color = RED;
+        node.left.color = BLACK;
+        node.right.color = BLACK;
+    }
+
+    public void draw(){
+        draw(root, 0);
+    }
+
+    private void draw(Node node, int level){
+        if(node == null){
+            return;
+        }
+
+        draw(node.right, level + 1);
+
+        for(int i = 0; i < level; i++){
+            StdOut.print("    ");
+        }
+
+        if(node.color == RED){
+            StdOut.println("(r)" + node.key);
+        }
+        else{
+            StdOut.println("(b)" + node.key);
+        }
+
+        draw(node.left, level + 1);
+
+    }
+
+    public void draw2() {
+        if (root == null) {
+            System.out.println("Дерево пустое");
+            return;
+        }
+        draw2(root, "", true);
+    }
+
+    private void draw2(Node node, String prefix, boolean isLeft) {
+        if (node == null) {
+            return;
+        }
+
+        if (node.right != null) {
+            draw2(node.right, prefix + (isLeft ? "│   " : "    "), false);
+        }
+
+        System.out.print(prefix);
+        System.out.print(isLeft ? "└── " : "┌── ");
+        System.out.print(node.key + (node.color == RED ? " (r)" : " (b)"));
+        System.out.print("\n");
+
+        if (node.left != null) {
+            draw2(node.left, prefix + (isLeft ? "    " : "│   "), true);
+        }
+    }
+
+}
---
+++ third-task-3.3/src/main/java/redblacktree/Task.java
@@ -0,0 +1,27 @@
+package redblacktree;
+
+import edu.princeton.cs.introcs.StdOut;
+
+public class Task {
+    public static void main(String[] args) {
+        RedBlackBST<Character, Character> redBlackBST = new RedBlackBST<>();
+
+        redBlackBST.add('E', 'E');
+        redBlackBST.add('A', 'A');
+        redBlackBST.add('S', 'S');
+        redBlackBST.add('Y', 'Y');
+        redBlackBST.add('Q', 'Q');
+        redBlackBST.add('U', 'U');
+        redBlackBST.add('T', 'T');
+        redBlackBST.add('I', 'I');
+        redBlackBST.add('O', 'O');
+        redBlackBST.add('N', 'N');
+
+        StdOut.println("r - red\nb - black\n");
+
+        redBlackBST.draw();
+        StdOut.println("---------------------------");
+        redBlackBST.draw2();
+
+    }
+}
